19bcb7c71bb81258e318300b9da953ca59aa3eda
Torham <TorhamDev@gmail.com>
2023-08-03 16:16:13 +0330

    Add more settings for simple-jwt

--- src/core/settings.py
+++ src/core/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -52,6 +53,16 @@ INSTALLED_APPS = [
     *LOCAL_APPS,
     *THIRD_PARTY_APPS,
 ]
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=4),
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
+    "UPDATE_LAST_LOGIN": True,
+    "AUTH_HEADER_TYPES": ("Bearer",),
+}
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
