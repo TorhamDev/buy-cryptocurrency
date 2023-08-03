86e22a5da5848d09f580f1ae170f40c23645ed1c
Torham <TorhamDev@gmail.com>
2023-08-03 15:02:34 +0330

    Add redis handler

---
+++ src/utils/redis_db.py
@@ -0,0 +1,14 @@
+import os
+import redis
+
+
+# redis configs
+REDIS_HOST = os.environ.get("REDIS_HOST", "127.0.0.1")
+REDIS_PORT = os.environ.get("REDIS_PORT", 6379)
+REDIS_DB = os.environ.get("REDIS_DB", 0)
+
+
+def get_redis_connection():
+    """redis database connection"""
+
+    return redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB)
