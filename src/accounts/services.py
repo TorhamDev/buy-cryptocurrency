862dc689574710ef914fd848ca2e4536e70a3178
Torham <TorhamDev@gmail.com>
2023-08-04 13:13:01 +0330

    Create wallet for user in register

--- src/accounts/services.py
+++ src/accounts/services.py
@@ -1,12 +1,16 @@
-from accounts.models import User
+from accounts.models import User, Wallet
 from django.db.models import QuerySet
 
 
 def create_user(phone_number, password) -> QuerySet:
     """
-    Creating user
+    Creating user and a wallt for it
     """
-    return User.objects.create_user(
+    user = User.objects.create_user(
         phone_number=phone_number,
         password=password,
     )
+
+    Wallet.objects.create(user=user)
+
+    return user
